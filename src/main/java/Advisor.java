7bff100c49073166d9b2e3764a015ccb4ed61a2c
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-01-21 20:11:40 +0800

    Add functionality for input

--- src/main/java/Advisor.java
+++ src/main/java/Advisor.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Advisor {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static String getCommand() {
+        System.out.println("Enter a command:");
+        return input.nextLine().strip().toLowerCase();
+    }
+
     public static void main(String[] args) {
         String logo = "            _       _                \n" +
                 "           | |     (_)               \n" +
@@ -12,16 +22,19 @@ public class Advisor {
         String line = "____________________________________________________________";
 
 
-
         System.out.println(logo);
         System.out.println(line);
-        System.out.println("Hello. I am "+ name);
+        System.out.println("Hello. I am " + name);
         System.out.println("What do you want me to do?");
 
+        String command = getCommand();
 
+
+        System.out.println(command);
 
         System.out.println(line);
         System.out.println("End of Session. Goodbye.");
+
 
     }
 }
