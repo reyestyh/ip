377923708d52b607f0a12506d36804eceb3ac504
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-01-22 10:03:43 +0800

    Add ToDoTask class

---
+++ src/main/java/ToDoTask.java
@@ -0,0 +1,13 @@
+public class ToDoTask extends Task {
+
+    public ToDoTask(String taskName) {
+        super(taskName);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+
+
+}
