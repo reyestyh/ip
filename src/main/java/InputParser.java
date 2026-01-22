f34123fbca4548723d09ff4b4cde896e253b9d19
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-01-22 11:13:03 +0800

    Add InputParser class

---
+++ src/main/java/InputParser.java
@@ -0,0 +1,20 @@
+public class InputParser {
+    public static String todoParser(String input) {
+        return input.substring(5);
+    }
+
+    public static String[] deadlineParser(String input) {
+        String temp = input.substring(9);
+        return temp.split(" /by ");
+    }
+
+    public static String[] eventParser(String input) {
+        String[] tmp = input.substring(6).split(" /from ");
+        String taskDesc = tmp[0];
+        String[] times =  tmp[1].split(" /to ");
+        String startTime = times[0];
+        String endTime = times[1];
+        return new String[]{taskDesc, startTime, endTime};
+    }
+
+}
