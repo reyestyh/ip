64c51f657c151d6cd28cd779dfc1c3dd0b9609f6
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-01-22 10:18:06 +0800

    Add EventTask class

---
+++ src/main/java/EventTask.java
@@ -0,0 +1,17 @@
+public class EventTask extends Task {
+
+    protected String startTime;
+    protected String endTime;
+
+    public EventTask(String taskName, String startTime, String endTime) {
+        super(taskName);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (from: " + this.startTime + " to: " + this.endTime + ")";
+    }
+
+}
