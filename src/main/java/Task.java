e4fcd280613037c9d38d270f0f63d5ce1a96f1d0
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-01-27 19:47:26 +0800

    Revert Task completion status to boolean

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,14 +3,8 @@
  */
 public class Task {
 
-    protected enum TaskStatus {
-        NOT_DONE,
-        IN_PROGRESS,
-        COMPLETED,
-    }
-
     protected String taskName;
-    protected TaskStatus finished;
+    protected boolean isFinished;
 
     /**
      * Constructor for Task object
@@ -18,21 +12,21 @@ public class Task {
      */
     public Task(String taskName) {
         this.taskName = taskName;
-        this.finished = TaskStatus.NOT_DONE;
+        this.isFinished = false;
     }
 
     /**
      * Marks Task object as complete
      */
     public void finishTask() {
-        this.finished = TaskStatus.COMPLETED;
+        this.isFinished = true;
     }
 
     /**
      * Marks Task object as incomplete
      */
     public void undo() {
-        this.finished = TaskStatus.NOT_DONE;
+        this.isFinished = false;
     }
 
 
@@ -40,7 +34,7 @@ public class Task {
      * @return boolean indicating task completion
      */
     public boolean isFinished() {
-        return this.finished == TaskStatus.COMPLETED;
+        return this.isFinished;
     }
 
 
