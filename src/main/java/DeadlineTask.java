e1c40df8209f0f3bd54054b324d17d6a753f85a4
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-01-27 21:39:22 +0800

    Add getDeadline() method

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -15,6 +15,10 @@ public class DeadlineTask extends Task {
         this.deadline = deadline;
     }
 
+    public String getDeadline() {
+        return deadline;
+    }
+
     @Override
     public String getTaskType() {
         return "D";
