bdef3e2d4ba1f4c341c22aedc4f289267fd71581
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-01-29 13:09:46 +0800

    Update javadoc comments

--- src/main/java/advisor/InputParser.java
+++ src/main/java/advisor/InputParser.java
@@ -8,9 +8,10 @@ public class InputParser {
 
     /**
      * Returns the index of the task to be deleted from user input
+     * If not a number or no number inputted, return -1
      *
      * @param input user input from delete command
-     * @return integer of index of Task in taskList, or -1 if there is an error
+     * @return integer of index of Task in taskList
      */
     public static Integer deleteParser(String input) {
         try {
@@ -22,9 +23,10 @@ public class InputParser {
 
     /**
      * Returns the index of the task to be marked as complete from user input
+     * If not a number or no number inputted, return -1
      *
      * @param input user input from mark command
-     * @return integer of index of Task in taskList, or -1 if there is an error
+     * @return integer of index of Task in taskList
      */
     public static Integer markParser(String input) {
         try {
@@ -36,9 +38,10 @@ public class InputParser {
 
     /**
      * Returns the index of the task to be marked as incomplete from user input
+     * If not a number or no number inputted, return -1
      *
      * @param input user input from unmark command
-     * @return integer of index of Task in taskList, or -1 if there is an error
+     * @return integer of index of Task in taskList
      */
     public static Integer unmarkParser(String input) {
         try {
@@ -65,9 +68,11 @@ public class InputParser {
     /**
      * Returns array of strings containing the description of the task and task deadline from
      * a deadline task
+     * index 0: task description
+     * index 1: deadline
      *
      * @param input user input from deadline command
-     * @return Array containing description of task (index 0) and deadline (index 1)
+     * @return String array of task data
      */
     public static String[] deadlineParser(String input) {
         String temp = "";
@@ -96,9 +101,12 @@ public class InputParser {
     /**
      * Returns array of strings containing the description of the task, start time and end time from
      * an event task
+     * index 0: task description
+     * index 1: start time
+     * index 2: end time
      *
      * @param input user input from event command
-     * @return Array containing description of task (index 0), start time (index 1), end time (index 2)
+     * @return String array of task data
      */
     public static String[] eventParser(String input) {
         String[] temp = null;
