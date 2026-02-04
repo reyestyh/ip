defe5615672e48cde4f969671bbc460448a0cfac
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-02-04 22:28:29 +0800

    Update tests

--- src/test/java/advisor/InputParserTest.java
+++ src/test/java/advisor/InputParserTest.java
@@ -3,25 +3,33 @@ package advisor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputParserTest {
 
     @Test
-    public void test1() {
-        assertEquals(-1, InputParser.deleteParser(""));
-        assertEquals(-1, InputParser.deleteParser("delete"));
-        assertEquals(-1, InputParser.deleteParser("delete "));
-        assertEquals(-1, InputParser.deleteParser("delete lol"));
+    public void deleteParser_incorrectFormat_exceptionThrown() throws AdvisorException {
+        assertThrows(AdvisorException.class, () -> InputParser.deleteParser(""));
+        assertThrows(AdvisorException.class, () -> InputParser.deleteParser("delete"));
+        assertThrows(AdvisorException.class, () -> InputParser.deleteParser("delete "));
+        assertThrows(AdvisorException.class, () -> InputParser.deleteParser("delete lol"));
+    }
+
+    @Test
+    public void deleteParser_correctFormat_correctNumberReturned() throws AdvisorException {
         assertEquals(7, InputParser.deleteParser("delete 7"));
     }
 
     @Test
-    public void test2() {
+    public void deadlineParser_incorrectFormat_nullReturned() {
         assertEquals(null, InputParser.deadlineParser("deadline"));
         assertEquals(null, InputParser.deadlineParser("deadline /by"));
         assertEquals(null, InputParser.deadlineParser("deadline huh"));
         assertEquals(null, InputParser.deadlineParser("deadline huh /by"));
+    }
 
+    @Test
+    public void deadlineParser_sampleCommand_correctParsing() {
         String[] temp1 = InputParser.deadlineParser("deadline do homework /by tomorrow");
         assertEquals("do homework", temp1[0]);
         assertEquals("tomorrow", temp1[1]);
@@ -30,7 +38,8 @@ public class InputParserTest {
         String[] temp2 = InputParser.deadlineParser("deadline do homework /by 2026-01-31 1800");
         assertEquals("do homework", temp2[0]);
         assertEquals("2026-01-31 1800", temp2[1]);
-
     }
+
+
 
 }
--- src/test/java/advisor/ToDoTaskTest.java
+++ src/test/java/advisor/ToDoTaskTest.java
@@ -7,21 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ToDoTaskTest {
 
     @Test
-    public void test1() {
+    public void todoTaskCreation_sampleTask_correctFormat() {
         ToDoTask temp = new ToDoTask("buy groceries");
         assertEquals("[T][ ] buy groceries", temp.toString());
         assertEquals(false, temp.isFinished());
 
+    }
+
+    @Test
+    public void todoTaskCreation_sampleTaskMark_correctFormat() {
+        ToDoTask temp = new ToDoTask("buy groceries");
         temp.finishTask();
         assertEquals("[T][X] buy groceries", temp.toString());
         assertEquals(true, temp.isFinished());
     }
 
     @Test
-    public void test2() {
+    public void todoTask_unmarkTask_correctOutput() {
         ToDoTask temp = new ToDoTask("do homework", true);
-        assertEquals("[T][X] do homework", temp.toString());
-        assertEquals(true, temp.isFinished());
 
         temp.undo();
         assertEquals("[T][ ] do homework", temp.toString());
