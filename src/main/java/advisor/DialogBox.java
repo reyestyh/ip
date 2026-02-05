63150513a7b31ebb7e9ce95643cebfef9b875649
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-02-05 16:24:43 +0800

    Add css stylesheets

--- src/main/java/advisor/DialogBox.java
+++ src/main/java/advisor/DialogBox.java
@@ -42,6 +42,7 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
     }
 
     /**
