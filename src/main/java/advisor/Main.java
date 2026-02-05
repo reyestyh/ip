3327b67440c5f59bd59745f7858ca5f51831e624
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-02-05 12:02:08 +0800

    Add skeleton GUI

---
+++ src/main/java/advisor/Main.java
@@ -0,0 +1,33 @@
+package advisor;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Advisor using FXML.
+ */
+public class Main extends Application {
+
+    private Advisor advisor = new Advisor();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            fxmlLoader.<MainWindow>getController().setAdvisor(advisor);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
