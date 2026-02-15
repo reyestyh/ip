6a8218d09564d9f492db37362dfb266dd91eb066
reyestyh <194571620+reyestyh@users.noreply.github.com>
2026-02-15 13:01:01 +0800

    Extract methods for commands

--- src/main/java/advisor/Advisor.java
+++ src/main/java/advisor/Advisor.java
@@ -57,143 +57,34 @@ public class Advisor {
 
         switch (command) {
         case "bye":
-            boolean isSuccessfulUpdate = this.taskList.updateStorage();
-
-            return userInterface.getUpdateFileMessage(isSuccessfulUpdate) + this.userInterface.getExitMessage();
-
+            return execByeCommand();
 
         case "list":
-            return this.userInterface.getCurrentTasks(this.taskList);
+            return execListCommand();
 
         case "sort":
-            this.taskList.sortTasks();
-            return this.userInterface.getSortedTasks(this.taskList);
+            return execSortCommand();
 
         case "mark":
-            int markIdx = 0;
-
-            try {
-                markIdx = InputParser.markParser(userInput);
-            } catch (AdvisorException e) {
-                return this.userInterface.getNotNumberMessage("mark");
-            }
-
-            // Converts index input from user to 0-based for code
-            markIdx -= 1;
-
-            try {
-                this.taskList.completeTask(markIdx);
-                Task fin = this.taskList.getTask(markIdx);
-                return this.userInterface.getMarkedMessage(fin);
-            } catch (IndexOutOfBoundsException e) {
-                return this.userInterface.getOutOfRangeMessage();
-            }
+            return execMarkCommand(userInput);
 
         case "unmark":
-            int unmarkIdx = 0;
-
-            try {
-                unmarkIdx = InputParser.unmarkParser(userInput);
-            } catch (AdvisorException e) {
-                return this.userInterface.getNotNumberMessage("unmark");
-
-            }
-
-            // Converts index input from user to 0-based for code
-            unmarkIdx -= 1;
-
-            try {
-                this.taskList.undoTask(unmarkIdx);
-                Task undone = this.taskList.getTask(unmarkIdx);
-                return this.userInterface.getUnmarkedMessage(undone);
-            } catch (IndexOutOfBoundsException e) {
-                return this.userInterface.getOutOfRangeMessage();
-            }
+            return execUnmarkCommand(userInput);
 
         case "todo":
-            String desc = "";
-            try {
-                desc = InputParser.todoParser(userInput);
-            } catch (AdvisorException e) {
-                return this.userInterface.getInvalidTodoMessage();
-            }
-            assert(!desc.isEmpty());
-
-            Task todoToAdd = new ToDoTask(desc);
-            updateToDoList(todoToAdd);
-            return this.userInterface.getNewTaskMessage(todoToAdd, this.taskList.getNumTasks());
-
+            return execTodoCommand(userInput);
 
         case "deadline":
-            int deadlineIndex = 1;
-
-            String[] deadlineData = InputParser.deadlineParser(userInput);
-            if (deadlineData == null) {
-                return this.userInterface.getInvalidDeadlineMessage();
-            }
-
-            Task deadlineToAdd = new DeadlineTask(deadlineData[descriptionIndex], deadlineData[deadlineIndex]);
-            try {
-                updateToDoList(deadlineToAdd);
-                return this.userInterface.getNewTaskMessage(deadlineToAdd, this.taskList.getNumTasks());
-
-            } catch (DateTimeParseException e) {
-                return this.userInterface.getInvalidDeadlineMessage();
-            }
-
+            return execDeadlineCommand(userInput, descriptionIndex);
 
         case "event":
-            int startTimeIndex = 1;
-            int endTimeIndex = 2;
-
-            String[] taskData = InputParser.eventParser(userInput);
-            if (taskData == null) {
-                return this.userInterface.getInvalidEventMessage();
-            }
-
-            Task eventToAdd = new EventTask(taskData[descriptionIndex],
-                                            taskData[startTimeIndex],
-                                            taskData[endTimeIndex]);
-            try {
-                updateToDoList(eventToAdd);
-                return this.userInterface.getNewTaskMessage(eventToAdd, this.taskList.getNumTasks());
-            } catch (DateTimeParseException e) {
-                return this.userInterface.getInvalidEventMessage();
-            }
-
+            return execEventCommand(userInput, descriptionIndex);
 
         case "delete":
-            int deleteIdx = 0;
-            try {
-                deleteIdx = InputParser.deleteParser(userInput);
-            } catch (AdvisorException e) {
-                return this.userInterface.getNotNumberMessage("delete");
-            }
-
-            // Converts index input from user to 0-based for code
-            deleteIdx -= 1;
-
-            try {
-                Task removed = taskList.deleteTask(deleteIdx);
-                return this.userInterface.getTaskDeletedMessage(removed, taskList.getNumTasks());
-            } catch (IndexOutOfBoundsException e) {
-                return this.userInterface.getOutOfRangeMessage();
-            }
+            return execDeleteCommand(userInput);
 
         case "find":
-
-            String term = "";
-            try {
-                term = InputParser.findParser(userInput);
-            } catch (AdvisorException e) {
-                return this.userInterface.getInvalidFindMessage();
-            }
-            assert(!term.isEmpty());
-
-            List<Task> matches = this.taskList.findTasks(term);
-
-            return this.userInterface.getFoundTasksMessage(matches, term);
-
+            return execFindCommand(userInput);
 
         default:
             return this.userInterface.getInvalidCommandMessage();
@@ -201,5 +92,155 @@ public class Advisor {
 
     }
 
+    private String execListCommand() {
+        return this.userInterface.getCurrentTasks(this.taskList);
+    }
+
+
+    private String execByeCommand() {
+        boolean isSuccessfulUpdate = this.taskList.updateStorage();
+        return userInterface.getUpdateFileMessage(isSuccessfulUpdate) + this.userInterface.getExitMessage();
+    }
+
+
+    private String execTodoCommand(String userInput) {
+        String desc = "";
+        try {
+            desc = InputParser.todoParser(userInput);
+        } catch (AdvisorException e) {
+            return this.userInterface.getInvalidTodoMessage();
+        }
+        assert (!desc.isEmpty());
+
+        Task todoToAdd = new ToDoTask(desc);
+        updateToDoList(todoToAdd);
+        return this.userInterface.getNewTaskMessage(todoToAdd, this.taskList.getNumTasks());
+    }
+
+
+    private String execDeadlineCommand(String userInput, int descriptionIndex) {
+        int deadlineIndex = 1;
+
+        String[] deadlineData = InputParser.deadlineParser(userInput);
+        if (deadlineData == null) {
+            return this.userInterface.getInvalidDeadlineMessage();
+        }
+
+        Task deadlineToAdd = new DeadlineTask(deadlineData[descriptionIndex], deadlineData[deadlineIndex]);
+        try {
+            updateToDoList(deadlineToAdd);
+            return this.userInterface.getNewTaskMessage(deadlineToAdd, this.taskList.getNumTasks());
+
+        } catch (DateTimeParseException e) {
+            return this.userInterface.getInvalidDeadlineMessage();
+        }
+    }
+
+
+    private String execEventCommand(String userInput, int descriptionIndex) {
+        int startTimeIndex = 1;
+        int endTimeIndex = 2;
+
+        String[] taskData = InputParser.eventParser(userInput);
+        if (taskData == null) {
+            return this.userInterface.getInvalidEventMessage();
+        }
+
+        Task eventToAdd = new EventTask(taskData[descriptionIndex],
+                taskData[startTimeIndex],
+                taskData[endTimeIndex]);
+        try {
+            updateToDoList(eventToAdd);
+            return this.userInterface.getNewTaskMessage(eventToAdd, this.taskList.getNumTasks());
+        } catch (DateTimeParseException e) {
+            return this.userInterface.getInvalidEventMessage();
+        }
+    }
+
+
+    private String execMarkCommand(String userInput) {
+        int markIdx = 0;
+
+        try {
+            markIdx = InputParser.markParser(userInput);
+        } catch (AdvisorException e) {
+            return this.userInterface.getNotNumberMessage("mark");
+        }
+
+        // Converts index input from user to 0-based for code
+        markIdx -= 1;
+
+        try {
+            this.taskList.completeTask(markIdx);
+            Task fin = this.taskList.getTask(markIdx);
+            return this.userInterface.getMarkedMessage(fin);
+        } catch (IndexOutOfBoundsException e) {
+            return this.userInterface.getOutOfRangeMessage();
+        }
+    }
+
+
+    private String execUnmarkCommand(String userInput) {
+        int unmarkIdx = 0;
+
+        try {
+            unmarkIdx = InputParser.unmarkParser(userInput);
+        } catch (AdvisorException e) {
+            return this.userInterface.getNotNumberMessage("unmark");
+
+        }
+
+        // Converts index input from user to 0-based for code
+        unmarkIdx -= 1;
+
+        try {
+            this.taskList.undoTask(unmarkIdx);
+            Task undone = this.taskList.getTask(unmarkIdx);
+            return this.userInterface.getUnmarkedMessage(undone);
+        } catch (IndexOutOfBoundsException e) {
+            return this.userInterface.getOutOfRangeMessage();
+        }
+    }
+
+
+    private String execDeleteCommand(String userInput) {
+        int deleteIdx = 0;
+        try {
+            deleteIdx = InputParser.deleteParser(userInput);
+        } catch (AdvisorException e) {
+            return this.userInterface.getNotNumberMessage("delete");
+        }
+
+        // Converts index input from user to 0-based for code
+        deleteIdx -= 1;
+
+        try {
+            Task removed = taskList.deleteTask(deleteIdx);
+            return this.userInterface.getTaskDeletedMessage(removed, taskList.getNumTasks());
+        } catch (IndexOutOfBoundsException e) {
+            return this.userInterface.getOutOfRangeMessage();
+        }
+    }
+
+
+    private String execFindCommand(String userInput) {
+        String term = "";
+        try {
+            term = InputParser.findParser(userInput);
+        } catch (AdvisorException e) {
+            return this.userInterface.getInvalidFindMessage();
+        }
+        assert (!term.isEmpty());
+
+        List<Task> matches = this.taskList.findTasks(term);
+
+        return this.userInterface.getFoundTasksMessage(matches, term);
+    }
+
+
+    private String execSortCommand() {
+        this.taskList.sortTasks();
+        return this.userInterface.getSortedTasks(this.taskList);
+    }
 
 }
